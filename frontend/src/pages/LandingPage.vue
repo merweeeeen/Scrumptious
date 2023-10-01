512bb2b87224ca3f740f09ba281371c7d1b05e65
merweeeeen <melvinngchuanjie@gmail.com>
2023-10-01 20:51:06 +0800

    Changed vite.config.js

--- frontend/src/pages/LandingPage.vue
+++ frontend/src/pages/LandingPage.vue
@@ -1,18 +1,5 @@
 <template>
   <v-app>
-    <v-card> NAVBAR </v-card>
-    <div style="margin-left: 10vw; margin-top: 5vh">
-      <v-row>
-        <v-col>
-          <v-card style="width: 10vw; height: 20vw; margin: 10px"> </v-card>
-        </v-col>
-      </v-row>
-    </div>
-    <div>
-      <v-row v-for="listing in listings" :key="listing.listing_id">
-        <ListingCard style="margin: 10px" :listing="listing"> </ListingCard>
-      </v-row>
-    </div>
     <Footer id="Footer"></Footer>
   </v-app>
 </template>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,19 +2,31 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith("v-"),
+export default defineConfig(({ mode }) => {
+  if (mode === "test") {
+    return {
+      plugins: [
+        vue({
+          template: {
+            compilerOptions: {
+              isCustomElement: (tag) => tag.startsWith("v-"),
+            },
+          },
+        }),
+      ],
+      base: "/Scrumptious/",
+      test: {
+        globals: true,
+        coverage: {
+          provider: "istanbul",
         },
+        environment: "happy-dom",
       },
-    }),
-  ],
-  base: "/Scrumptious/",
-  test: {
-    globals: true,
-    environment: "happy-dom",
-  },
+    };
+  } else {
+    return {
+      plugins: [vue()],
+      base: "/Scrumptious/",
+    };
+  }
 });
