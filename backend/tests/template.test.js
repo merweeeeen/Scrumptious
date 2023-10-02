fae66cdc0a6031c49729fd2ab17ecfe684780920
merweeeeen <melvinngchuanjie@gmail.com>
2023-10-02 17:58:14 +0800

    Update test files

--- backend/tests/template.test.js
+++
@@ -1,3 +0,0 @@
-test("template", () => {
-    expect(1+1).toBe(2)
-})
--- frontend/tests/test.test.js
+++ frontend/tests/test.test.js
@@ -1,11 +1,5 @@
-import { mount } from "@vue/test-utils";
-import Footer from "../src/components/Footer.vue"; // Import the component you need
-
-describe("Testing buttons", () => {
-  test("Home button", () => {
-    const wrapper = mount(Footer); // Mounting the Footer
-    const button = wrapper.find("#Home"); // Finding the id 'Home' in the Footer component
-
-    expect(Object.keys(button).length).toBeGreaterThan(0); // If the id Home is not found, the length would be 0
-  });
+describe("Template Describe", () => {
+  test('Template Test', () => {
+    expect(1+1).toBe(2)
+  })
 });
