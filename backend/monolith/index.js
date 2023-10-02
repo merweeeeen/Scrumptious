72f49a5d3a6b419577b35c3e0bb953bfe19d7e79
lixuennn <llx16702@gmail.com>
2023-10-02 23:42:16 +0800

    Removed Old Structure, Completed Monolith Role Functionalities
    Removed Old Structure, Completed Monolith Role Functionalities

--- backend/monolith/index.js
+++ backend/monolith/index.js
@@ -1,19 +1,25 @@
 const express = require("express");
-
 const app = express();
 const port = 3003;
 const bodyParser = require("body-parser");
-
-
 app.use(bodyParser.json());
+
 
 const role = require("./role");
 
+
+/////////////////////////////////////////////////////
+///////////////////// TEST //////////////////////////
 
 app.get('/', (req, res) => {
   res.send('Testing Hello World!')
 })
 
+/////////////////////////////////////////////////////
+////////////// ROLE MICROSERVICE ////////////////////
+/////////////////////////////////////////////////////
+
+// THIS IS GET /role => TO GET ALL ROLES FOR FRONTEND
 app.get('/role', async (req, res) => {
     role.readAllRole()
   .then((results) => {
@@ -27,6 +33,53 @@ app.get('/role', async (req, res) => {
     // res.send(results)
 })
 
+// THIS IS GET /role/:roleid? => TO GET ONE ROLE FOR FRONTEND
+app.get('/role/:roleid?', async (req, res) => {
+    console.log(req.params.roleid)
+    role.readOneRole(req.params.roleid)
+  .then((results) => {
+    console.log("Results: ", results);
+    res.send(results)
+  })
+  .catch((error) => {
+    console.error("Error: ", error);
+  });
+})
+
+// THIS IS GET /listing/:listingid? => TO GET ONE ROLE FOR FRONTEND
+app.get('/listing/:listingid?', async (req, res) => {
+    console.log(req.params.listingid)
+    role.readOneListing(req.params.listingid)
+  .then((results) => {
+    console.log("Results: ", results);
+    res.send(results)
+  })
+  .catch((error) => {
+    console.error("Error: ", error);
+  });
+})
+
+// THIS IS POST /listing => TO CREATE A ROLE
+// {"listing_name":"ListName1","role_id":1,"role_name":"RoleName1","dept":"asdas","country":"sg","num_openings":2,"expiry_date":"2023-07-04","open":1, "desc":"desc1"}
+app.post('/listing', async (req, res) => {
+    console.log(req.body)
+    role.createRoleListing(req.body)
+    .then((results) => {
+        console.log("Results: ", results);
+        res.send(results)
+    })
+    .catch((error) => {
+        console.error("Error: ", error);
+    });
+})
+/////////////////////////////////////////////////////
+////////////// ROLE_SKILL MICROSERVICE //////////////
+/////////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
--- backend/monolith/role.js
+++ backend/monolith/role.js
@@ -33,9 +33,9 @@ async function readAllRole() {
     });
   }
 
-function readOneRole(listingid) {
+function readOneRole(roleid) {
     return new Promise((resolve, reject) => {
-        const query = `SELECT * FROM listing WHERE role_id = ${listingid};`;
+        const query = `SELECT * FROM listing WHERE role_id = ${roleid};`;
         con.query(query, function (error, results, fields) {
             if (error) {
                 reject(error);
@@ -47,40 +47,52 @@ function readOneRole(listingid) {
    });
 }
 
-// POST, should take in an object like this: {"role_name":"TestName","dept":"asdas","country":"sg","num_openings":2,"expiry_date":"2023-07-04","open":1}
-function createRole(theBody) {
-    const roleName = theBody.role_name;
-    const dept = theBody.dept;
-    const country = theBody.country;
-    const numOpenings = theBody.num_openings;
-    const expiryDate = theBody.expiry_date; // YYYY-MM-DD format
-    const openVal = theBody.open;
-    const currentdate = new Date(); 
-    const datetime = currentdate.getFullYear() + "-"
-                + (currentdate.getMonth()+1)  + "-" 
-                + currentdate.getDate() + " "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-    console.log(datetime)
-    // YYYY-MM-DD HH:MI:SS format
-    const query = `INSERT INTO role (role_name, dept, country, num_openings, expiry_date, open, created_date) VALUES ('${roleName}', '${dept}', '${country}', '${numOpenings}', '${expiryDate}', '${openVal}', '${datetime}');`;
-    con.query(query, function (error, results, fields) {
-      if (error) throw error;
-      console.log("success");
-      console.log(results)
-      // OkPacket {
-      //   fieldCount: 0,
-      //   affectedRows: 1,
-      //   insertId: 10,
-      //   serverStatus: 2,
-      //   warningCount: 0,
-      //   message: '',
-      //   protocol41: true,
-      //   changedRows: 0
-      // }
-      return results;
+function readOneListing(listingid) {
+    return new Promise((resolve, reject) => {
+        const query = `SELECT * FROM listing WHERE listing_id = ${listingid};`;
+        con.query(query, function (error, results, fields) {
+            if (error) {
+                reject(error);
+              } else {
+              //   console.log("results: " + results)         
+                resolve(results);
+        }
+      });
+   });
+}
+
+// POST, should take in an object like this: {"listing_name":"ListName1","role_id":1,"role_name":"RoleName1","dept":"asdas","country":"sg","num_openings":2,"expiry_date":"2023-07-04","open":1, "desc":"desc1"}
+function createRoleListing(theBody) {
+    return new Promise((resolve, reject) => {
+        const listingName = theBody.listing_name;
+        const roleId = theBody.role_id;
+        const roleName = theBody.role_name;
+        const dept = theBody.dept;
+        const country = theBody.country;
+        const numOpenings = theBody.num_openings;
+        const expiryDate = theBody.expiry_date; // YYYY-MM-DD format
+        const openVal = theBody.open;
+        const currentdate = new Date(); 
+        const datetime = currentdate.getFullYear() + "-"
+                    + (currentdate.getMonth()+1)  + "-" 
+                    + currentdate.getDate() + " "  
+                    + currentdate.getHours() + ":"  
+                    + currentdate.getMinutes() + ":" 
+                    + currentdate.getSeconds();
+        console.log(datetime) // YYYY-MM-DD HH:MI:SS format
+        const desc = theBody.desc;
+        const query = `INSERT INTO listing (listing_name, role_id, role_name, dept, country, num_openings, expiry_date, open, created_date, description) VALUES ('${listingName}','${roleId}','${roleName}', '${dept}', '${country}', '${numOpenings}', '${expiryDate}', '${openVal}', '${datetime}', '${desc}');`;
+        con.query(query, function (error, results, fields) {
+            if (error) {
+                console.log("error: " + error)
+                reject(error);
+            } else {
+                console.log("results: " + results)         
+                resolve(results);
+        }
+        });
     });
   }
+// createRoleListing({listing_name:"ListName1",role_id:1, role_name:"RoleName1",dept:"asdas",country:"sg",num_openings:2,expiry_date:"2023-07-04",open:1, desc:"desc1"})
 
-  module.exports = { readAllRole, readOneRole, createRole };
+  module.exports = { readAllRole, readOneRole, readOneListing, createRoleListing };
