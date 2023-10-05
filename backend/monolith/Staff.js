dc406979e17356d8dd70f878bf29a9d67f2be498
bryanleezh <leebryan307@gmail.com>
2023-10-05 14:09:17 +0800

    updated Staff.js

--- backend/monolith/Staff.js
+++ backend/monolith/Staff.js
@@ -9,38 +9,38 @@ class Staff {
         Access_Rights, 
         Skills
         ) {
-        this.Staff_id = Staff_id;
-        this.Staff_FName = Staff_FName;
-        this.Staff_LName = Staff_LName;
-        this.Dept = Dept;
-        this.Country = Country;
-        this.Email = Email;
-        this.Access_Rights = Access_Rights;
-        this.Skills = Skills;
+        this._Staff_id = Staff_id;
+        this._Staff_FName = Staff_FName;
+        this._Staff_LName = Staff_LName;
+        this._Dept = Dept;
+        this._Country = Country;
+        this._Email = Email;
+        this._Access_Rights = Access_Rights;
+        this._Skills = Skills;
     }
 
     get Staff_id() {
-        return this.Staff_id;
+        return this._Staff_id;
     }
     get Staff_FName(){
-        return this.Staff_FName;
+        return this._Staff_FName;
     }
     get Staff_LName(){
-        return this.Staff_LName;
+        return this._Staff_LName;
     }
     get Dept(){
-        return this.Dept;
+        return this._Dept;
     }
     get Country(){
-        return this.Country;
+        return this._Country;
     }
     get Email(){
-        return this.Email;
+        return this._Email;
     }
     get Access_Rights(){
-        return this.Access_Rights;
+        return this._Access_Rights;
     }
     get Skills(){
-        return this.Skills;
+        return this._Skills;
     }
 }
