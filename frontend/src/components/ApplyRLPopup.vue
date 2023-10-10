9e124129593606a171a68680df81a7c3e3968010
renee7777 <reneelim.2021@scis.smu.edu.sg>
2023-10-10 18:44:16 +0800

    Popup for Role listing application from ListingCard created

---
+++ frontend/src/components/ApplyRLPopup.vue
@@ -0,0 +1,178 @@
+<template>
+    <div class="text-center">
+        <v-btn
+        color="primary"
+        width = 100%
+        height="100%"
+        density="comfortable"
+        size="large"
+        variant="flat" 
+        id = "Apply"
+        >
+        Apply
+
+        <v-dialog
+            v-model="dialog"
+            activator="parent"
+            width="700"
+        >
+            <v-card>
+            <v-card-title>Apply for role {{roleName}}</v-card-title>
+            <v-card-item>
+                <div>
+                    <!--Staff's personal details so they can check that the dettails-->
+                    <!--are correct before applying-->
+                    <div class="text-h6 mb-1">
+                        Contact Info
+                    </div>
+                    <div class="text">
+                        {{ staffName }}
+                    </div>
+                    <div class="text-caption">
+                        {{ Department }} | {{ currRole }}
+                    </div>
+                    <div class="text-caption">
+                        E-mail address: {{ email }} | Phone number: {{phone}}
+                    </div>
+
+
+                    <div class="text-h6">
+                        Resume
+                    </div>
+                    <div class="text-caption mb-1">
+                        Be sure to include an updated resume*
+                    </div>
+                    
+                    <!--Select previously used resume-->
+                    <!-- <v-combobox
+                    clearable
+                    chips
+                    label="Select Resume"
+                    :items="['JohnDoe_Resume', 'JohnDoe_Resume1']"
+                    variant="outlined"
+                    ></v-combobox> -->
+
+                    <!-- <div class="text">
+                        Upload Resume
+                    </div> -->
+                    <!-- <v-btn color="primary" 
+                        class="text-none text-subtitle-1"
+                        size="small"
+                        variant="outlined" 
+                        @click="dialog = false">Upload Resume
+                    </v-btn> -->
+
+                    <!--for staff to upload an updated resume-->
+                    <v-file-input
+                        chips
+                        clearable
+                        accept="image/png, image/jpeg, image/bmp, application/pdf"
+                        label="Upload Resume"
+                        density = "compact"
+                        color="deep-purple-accent-4"
+                    ></v-file-input>
+                </div>
+            </v-card-item>
+
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                    <!-- <v-col
+                        cols="12"
+                        sm="3"
+                        md="3"
+                    > -->
+                    <!--Section for staff to include more details for their application-->
+                    <div>
+                        <div class="text mr-1">
+                            Write-up
+                        </div>
+                    </div>
+                    <!-- </v-col> -->
+                    <!-- <v-col
+                        cols="12"
+                        sm="6"
+                        md="9"
+                    > -->
+                        <v-text-field
+                        colour="deep-purple-accent-4"
+                        label="Write a brief summary of why you should be chosen for this role"
+                        ></v-text-field>
+                    <!-- </v-col> -->
+                    </v-row>
+                </v-container>
+            </v-card-text>
+
+            <!--Submit button-->
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" 
+                class="text-none text-subtitle-1"
+                size="small"
+                variant="flat" 
+                @click="dialog2 = false"
+                >
+                    Submit
+                    <!--pop up for confirmation of successful submission-->
+                    <v-dialog
+                    v-model="dialog2"
+                    activator="parent"
+                    width="500"
+                    persistent
+                    >
+                    <v-card>
+                    <!-- <v-card append-icon = "$close"> -->
+                        <!-- <template v-slot:append>
+                            <v-btn icon="$close" variant="text" @click="dialog2 = false"></v-btn>
+                        </template> -->
+                        <div class="py-12 mx-9 text-center">
+                            <v-icon
+                                class="mb-6"
+                                color="success"
+                                icon="mdi-check-circle-outline"
+                                size="120"
+                            ></v-icon>
+                            <div class="text-h6 font-weight-bold">Application Submitted Successfully!</div>
+                            <div class="text">Thank you for your application, kindly look out for an update on your application status which will be sent via email</div>
+                        </div>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="primary" 
+                            class="text-none text-subtitle-1"
+                            size="small"
+                            variant="flat" 
+                            @click="dialog2 = false;dialog = false"
+                            >
+                            Close
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                    </v-dialog>
+                </v-btn>
+            </v-card-actions>
+
+        </v-card>
+        </v-dialog>
+        </v-btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ApplyRLPopup",
+        data () {
+        return {
+            dialog: false,
+            dialog2: false,
+        }
+        },
+        props: {
+            roleName: String,
+            Department: String,
+            currRole: String,
+            staffName: String,
+            email: String,
+            phone: String,
+        },
+    }
+</script>
--- frontend/src/components/Listing.vue
+++ frontend/src/components/Listing.vue
@@ -13,7 +13,7 @@
         > 
         <!-- Role Name and when done, bookmark/3 dot icon -->
         <v-card-item class="mt-0 w-100">
-          <v-container class="pa-0 mt-1">
+        <v-container class="pa-0 mt-1">
             <v-row no-gutters align="center" style="height: 25px;">
                 <v-col class="pa-0">
                     <v-card-title>{{roleName}}</v-card-title>
@@ -65,7 +65,7 @@
             </v-row>
             <v-row>
                 <v-col>
-                    <v-btn 
+                    <!-- <v-btn 
                     width="100%"
                     height="100%"
                     density="comfortable"
@@ -73,7 +73,8 @@
                     variant="flat"
                     v-bind:color="primaryColor"
                     id="apply"
-                    >Apply</v-btn>
+                    >Apply</v-btn> -->
+                    <ApplyRLPopup/>
                 </v-col>
             </v-row>
         </v-container>
@@ -81,159 +82,164 @@
         </v-card-item>
 
     <v-expand-transition>
-      <div v-show="show">
+    <div v-show="show">
         <v-divider></v-divider>
-      </div>
+    </div>
     </v-expand-transition>
-      </v-card> 
-      <v-card 
-            width="100%"
-            min-width="400px"
-            color="red" 
-            variant="flat"
-            onmouseover="this.style.boxShadow='0 0 10px 0 rgba(0,0,0,0.5)'; show=true"
-            onmouseleave="this.style.boxShadow='none';show=false;"
-            v-if="open == 0 && access == 'HR'"
-        > 
-        <!-- Role Name and when done, bookmark/3 dot icon -->
-        <v-card-item class="mt-0 w-100">
-          <v-container class="pa-0 mt-1">
-            <v-row no-gutters align="center" style="height: 25px;">
-                <v-col class="pa-0">
-                    <v-card-title>{{roleName}}</v-card-title>
-                </v-col>
-                
-                <!-- This should be where the bookmark/3 dot icon should go -->
-                <v-col class="pa-0">
-                    <!-- <v-icon icon="fa:fas fa-list"></v-icon> -->
-                    <!-- <v-text class="text-caption text-grey-darken-2">More</v-text> -->
-                </v-col>
-            </v-row>
-        </v-container>
-
-        <!-- Info about role: Department, Openings, Applicants, Full Time -->
-        <v-text 
-                class="text-caption"
-            >
-            {{Department}} | {{num_openings}} Opening(s)
-        </v-text>
-
-        <!-- Skills Matched Percentage Bar -->
-        <!-- <v-container class="pa-0 mt-1">
-            <v-row no-gutters align="center" style="height: 25px;">
-                <v-text class="text-subtitle-1">Skills Matched</v-text>
-            </v-row>
-            <v-row no-gutters align="center" style="height: 25px;">
-            <v-col class="pa-1">
-                <v-sheet class="rounded-5 bg-transparent" style="border: solid 1px black; border-radius: 10px;">
-                    <v-progress-linear 
-                        v-bind:model-value="skillsPctMatch"
-                        v-bind:bg-color="primaryColor"
-                        v-bind:color="secondaryColor"
-                        rounded
-                        :height="15"
-                    >
-                    <v-text class="text-caption">{{getSkillsPctMatch(this.employeeSkills, this.roleInfo.Skills_Required)}}%</v-text>
-                </v-progress-linear>
-                </v-sheet>
+    </v-card> 
+    <v-card 
+        width="100%"
+        min-width="400px"
+        color="red" 
+        variant="flat"
+        onmouseover="this.style.boxShadow='0 0 10px 0 rgba(0,0,0,0.5)'; show=true"
+        onmouseleave="this.style.boxShadow='none';show=false;"
+        v-if="open == 0 && access == 'HR'"
+    > 
+    <!-- Role Name and when done, bookmark/3 dot icon -->
+    <v-card-item class="mt-0 w-100">
+    <v-container class="pa-0 mt-1">
+        <v-row no-gutters align="center" style="height: 25px;">
+            <v-col class="pa-0">
+                <v-card-title>{{roleName}}</v-card-title>
             </v-col>
-            </v-row>
-        </v-container>
-    -->
-        <!-- Last Updated -->
-        <v-container class="pa-0">
-            <v-row no-gutters justify="space-between" style="height: 25px;">
-                <v-col cols="auto">
-                    <v-text class="text-caption">{{ days_posted(created_at) }}</v-text>
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col>
-                    <v-btn 
-                    width="100%"
-                    height="100%"
-                    density="comfortable"
-                    size="large"
-                    variant="flat"
-                    v-bind:color="primaryColor"
-                    id="apply"
-                    disabled
-                    >Apply</v-btn>
-                </v-col>
-            </v-row>
-        </v-container>
+            
+            <!-- This should be where the bookmark/3 dot icon should go -->
+            <v-col class="pa-0">
+                <!-- <v-icon icon="fa:fas fa-list"></v-icon> -->
+                <!-- <v-text class="text-caption text-grey-darken-2">More</v-text> -->
+            </v-col>
+        </v-row>
+    </v-container>
 
-        </v-card-item>
+    <!-- Info about role: Department, Openings, Applicants, Full Time -->
+    <v-text 
+            class="text-caption"
+        >
+        {{Department}} | {{num_openings}} Opening(s)
+    </v-text>
+
+    <!-- Skills Matched Percentage Bar -->
+    <!-- <v-container class="pa-0 mt-1">
+        <v-row no-gutters align="center" style="height: 25px;">
+            <v-text class="text-subtitle-1">Skills Matched</v-text>
+        </v-row>
+        <v-row no-gutters align="center" style="height: 25px;">
+        <v-col class="pa-1">
+            <v-sheet class="rounded-5 bg-transparent" style="border: solid 1px black; border-radius: 10px;">
+                <v-progress-linear 
+                    v-bind:model-value="skillsPctMatch"
+                    v-bind:bg-color="primaryColor"
+                    v-bind:color="secondaryColor"
+                    rounded
+                    :height="15"
+                >
+                <v-text class="text-caption">{{getSkillsPctMatch(this.employeeSkills, this.roleInfo.Skills_Required)}}%</v-text>
+            </v-progress-linear>
+            </v-sheet>
+        </v-col>
+        </v-row>
+    </v-container>
+-->
+    <!-- Last Updated -->
+    <v-container class="pa-0">
+        <v-row no-gutters justify="space-between" style="height: 25px;">
+            <v-col cols="auto">
+                <v-text class="text-caption">{{ days_posted(created_at) }}</v-text>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-btn 
+                width="100%"
+                height="100%"
+                density="comfortable"
+                size="large"
+                variant="flat"
+                v-bind:color="primaryColor"
+                id="apply"
+                disabled
+                >Apply</v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+
+    </v-card-item>
 
     <v-expand-transition>
-      <div v-show="show">
+        <div v-show="show">
         <v-divider></v-divider>
-      </div>
+        </div>
     </v-expand-transition>
-      </v-card> 
+    </v-card> 
     </div>
 
     </div> 
-  </template> 
+</template> 
 
 <script>
+import ApplyRLPopup from './ApplyRLPopup.vue';
+
 export default {
     name: "ListingCard",
-  props: {
-    roleName: String,
-    Department: String,
-    num_openings: Number,
-    created_at: String,
-    open: Number,
-    access: String,
-    // employeeSkills: Array,
-    // lastUpdated: String,
-    // primaryColor: String,
-    // secondaryColor: String
-  },
-  data() {
-    // skillsPctMatch = getSkillsPctMatch(this.employeeSkills, this.employeeSkills);
-    
-    return {
-        show: false,
-        employeeSkills: ["Python", "C++"],
-        skillsPctMatch: "",
-        primaryColor: "grey",
-        secondaryColor: "grey-lighten-1",
-        // access: (access) => store.commit("access", access)
-    };
-  },
-  methods: {
-    // getSkillsPctMatch(employeeSkills, roleSkills) {
-    //     var numSkillsMatched = 0;
-    //     for (var i = 0; i < employeeSkills.length; i++) {
-    //         if (roleSkills.includes(employeeSkills[i])) {
-    //             numSkillsMatched++;
-    //         }
-    //     }
+    props: {
+        roleName: String,
+        Department: String,
+        num_openings: Number,
+        created_at: String,
+        open: Number,
+        access: String,
+        // employeeSkills: Array,
+        // lastUpdated: String,
+        // primaryColor: String,
+        // secondaryColor: String
+    },
+    data() {
+        // skillsPctMatch = getSkillsPctMatch(this.employeeSkills, this.employeeSkills);
+        
+        return {
+            show: false,
+            employeeSkills: ["Python", "C++"],
+            skillsPctMatch: "",
+            primaryColor: "grey",
+            secondaryColor: "grey-lighten-1",
+            // access: (access) => store.commit("access", access)
+        };
+    },
+    methods: {
+        // getSkillsPctMatch(employeeSkills, roleSkills) {
+        //     var numSkillsMatched = 0;
+        //     for (var i = 0; i < employeeSkills.length; i++) {
+        //         if (roleSkills.includes(employeeSkills[i])) {
+        //             numSkillsMatched++;
+        //         }
+        //     }
 
-    //     // The line below updates the skillsPctMatch variable
-    //     this.skillsPctMatch = Math.round((numSkillsMatched / roleSkills.length) * 100);
-    //     return Math.round((numSkillsMatched / roleSkills.length) * 100);
-    // },
-    days_posted(created_at) {
-        var today = new Date();
-        var created = new Date(created_at);
-        var diff = today - created;
-        var days = Math.floor(diff / (1000 * 60 * 60 * 24));
-        if (days == 1) {
-            return "Posted " + days + " day ago";
+        //     // The line below updates the skillsPctMatch variable
+        //     this.skillsPctMatch = Math.round((numSkillsMatched / roleSkills.length) * 100);
+        //     return Math.round((numSkillsMatched / roleSkills.length) * 100);
+        // },
+        days_posted(created_at) {
+            var today = new Date();
+            var created = new Date(created_at);
+            var diff = today - created;
+            var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+            if (days == 1) {
+                return "Posted " + days + " day ago";
+            }
+            else if (days == 0) {
+                return "Posted today";
+            }
+            else {
+            return "Posted " + days + " days ago";
+            }
         }
-        else if (days == 0) {
-            return "Posted today";
-        }
-        else {
-        return "Posted " + days + " days ago";
-        }
-}
-  },
+    },
     mounted() {
         console.log(this.access);
+    },
+    components: {
+        ApplyRLPopup,
     }
 };
 
