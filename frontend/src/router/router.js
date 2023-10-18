6ac0682c9d5eb5c8e1242a5a75e871d44da6cd1b
merweeeeen <melvinngchuanjie@gmail.com>
2023-10-18 15:42:31 +0800

    Update routing

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,39 +1,47 @@
-
-import {createRouter, createWebHistory} from 'vue-router';
-import LandingPage from '../views/LandingPage.vue';
-import CreateListing from '../views/CreateListing.vue';
-import ListingPage from '../views/ListingPage.vue';
-import Login from '../views/Login.vue';
-
+import { createRouter, createWebHistory } from "vue-router";
+import LandingPage from "../views/LandingPage.vue";
+import CreateListing from "../views/CreateListing.vue";
+import ListingPage from "../views/ListingPage.vue";
+import Login from "../views/Login.vue";
+import Test from '../views/Test.vue'
+import authGuard from "./authGuard";
 
 const routes = [
-      {
-        path: '/:skills?/:vacancy?/:dept?/:roleName?',
-        name: 'LandingPage',
-        component: LandingPage,
-      },
-      {
-        path: '/login',
-        name: 'Login',
-        component: Login,
-      },
-      {
-        path: '/create',
-        name: 'CreatePage',
-        component: CreateListing,
-      },
-      {
-        path: '/listing/:listing_id',
-        name: 'ListingPage',
-        component: ListingPage,
-      }
-    ]
+  {
+    path: "/:skills?/:vacancy?/:dept?/:roleName?",
+    name: "LandingPage",
+    component: LandingPage,
+    beforeEnter: authGuard
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/create",
+    name: "CreatePage",
+    component: CreateListing,
+    beforeEnter: authGuard
+  },
+  {
+    path: "/listing/:listing_id",
+    name: "ListingPage",
+    component: ListingPage,
+    beforeEnter: authGuard
+  },
+  {
+    path:"/listing",
+    name: "Test",
+    component: Test
+  }
+];
 
 const router = createRouter({
-    history: createWebHistory('/Scrumptious'),
+  history: createWebHistory("/Scrumptious"),
   routes,
 });
 
-export { routes }
+export { routes };
 
 export default router;
